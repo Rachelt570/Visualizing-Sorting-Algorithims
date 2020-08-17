77c5d75ca6579fc8d18f83b6801adf705917f317
Rachel Casey <rtkc.casey@gmail.com>
2020-08-16 23:41:18 -0600

    Finished bubblesort

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,70 +1,101 @@
-
-var canvasHeight;
-var canvasWidth;
-var elementCount;
-var values = [];
-var states = []; 
-var minimum;
-var maximum;
-var sortSpeed;
-var barSpred;
-
 var sortMethod;
+let values = [];
+let states = []; 
+let elementCount = 25; 
 
-
-Array.prototype.swap = function(a, b) {
-	let input = this;
-	let tmp = input[a];
-	input[a] = input[b];
-	input[b] = tmp;
-
-}
-
-function setup() {
-	frameRate(2);
-	noStroke();
-
-	sortSpeed = 2;
-	minimum = 50;
-	maximum = 300;
-	elementCount = 50;
-	barSpred = (3);
-	canvasWidth = windowWidth;
-	canvasHeight = windowHeight/2;
-	for (let i = 0; i < elementCount; i++)
-	{
-		values.push(new int);
-		values[i] = random(maximum-minimum) + minimum;
-	}
-
-
-	createCanvas(canvasWidth, canvasHeight);
-}
-
-async function swap(array, a, b)
-{
-		await sleep(30);	
-		let temp = 	array[a];
-		array[a] = array[b];
-		array[b] = temp; 
-}
-
-
-function sleep(milliseconds) { 
-        return new Promise(resolve => setTimeout(resolve, milliseconds)); 
+function setup() 
+{ 
+	elementWidth = (displayWidth / elementCount);
+	createCanvas(displayWidth, displayHeight/2);           
+    values = new Array(floor(width/elementWidth)); 
+    for(let i = 0; i < values.length; i++) 
+    { 
+    	values[i] = float(random(height)); 
+        states[i] = -1;  
+    }   
+    bubbleSort(values, 0, values.length);        
 } 
-function draw() {
 
-	values = sort(values, values.length);
-	background(0);
-	stroke(255);		
-	var barWidth = (canvasWidth / (elementCount) - 1);
-	var x = 0;
-	barSpred = 1;          
-	for (let i = 0; i < values.length; i++)
-	{
-		rect(x, canvasHeight, barWidth - barSpred, values[i] - canvasHeight);	
-		x+=(barSpred + barWidth);
+async function bubbleSort(arr, start, end) 
+{ 
+	if(start >= end) 
+	{ 
+    	return; 	
+    } 
+    for(var i = 0; i < end-1; i++)
+    { 
+    	for(var n = 0; n < end-i-1; n++) 
+    	{ 
+            if(arr[n] >= arr[n+1]) 
+            { 
+                states[n] = 1; 
+                await swap(arr, n, n+1); 
+                states[n+1] = 0; 
+            } 
+            states[n] = 2; 
+        } 
+    }
+	return arr; 
+} 
+
+function draw() 
+{ 
+    if(isSorted(values)) 
+    {
+    	stroke(0);
+        fill("#00ff00");
+        background(100);
+        for(let i = 0; i < values.length; i++) 
+        {
+        	rect(i*elementWidth, height - values[i], elementWidth, values[i]);
+        }
+    }
+    else 
+    {
+		background(100); 
+		for(let i = 0; i < values.length; i++) 
+		{ 
+	    	stroke(0); 
+			fill(255); 
+			if(states[i] == 0) 
+			{ 
+				fill(255, 0, 0); 
+			} 
+			else if (states[i] == 1) 
+			{ 
+				// Element being sorted
+				fill("#ff0000"); 
+			} 
+			else 
+			{ 
+				fill(255); 
+			} 
+			rect(i*elementWidth, height - values[i], elementWidth, values[i]); 
+		} 
 	}
+} 
 
+async function swap(arr, a, b) 
+{ 
+	await sleep(20); 
+	let tmp = arr[a]; 
+	arr[a] = arr[b]; 
+	arr[b] = tmp; 
 }
+
+function isSorted(arr) 
+{
+	for(let i = 0; i < arr.length-1; i++) 
+	{
+		if(arr[i] > arr[i+1]) 
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+function sleep(ms) 
+{ 
+	return new Promise(resolve => setTimeout(resolve, ms)); 
+} 
