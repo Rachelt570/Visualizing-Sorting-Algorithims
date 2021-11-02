9179d5d1656ab8c6e1d04e4a407f8b49235f8bfb
Rachel Casey <rtkc.casey@gmail.com>
2021-11-01 23:31:24 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,69 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Visualizing Sorting Algorithms</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="assets/css/style.css">
-	 	<script src = "assets/js/jquery.js"></script>
+	<title> Visualizing Sorting Algorithms </title>
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<script type="text/javascript" src="scripts/js/p5.min.js"></script>
+	<script type = "text/javascript" src="scripts/js/jquery.js"> </script>
 
 </head>
 <body>
-<header>
-	<nav> 
-		<input type = "button" value = "Generate Array" id = "generateArray">
-		<span> 
-			Number of Elements
-			<input type = "range" min = "2" value="25"  max = "50" id = "elementCount">
-			<button id = "elementCounter" value = "25" > 25 </button>
-		</span> 
-		<span> 
-			Sorting Speed
-			<input type = "range" min = "1" value = "50" max = "100" id = "sortingSpeed">
-			<button id = "sortingSpeedCounter" value = "50"> 50 </button>
-		</span>
+	<header>
+		<h1> Sorting Algorithim Visualization Tool </h1>
+		<form> 
+			<label for = "elementCount"> Element Count </label>
+			<input id = "elementCount" type = "range" min = "2" max = "100" value = "50" oninput = "elementCountUpdate(this.value)">
+			<label for = "sortSpeed"> Sorting Speed </label> 
+			<input id = "sortSpeed" type = "range" min = "1" max = "60" value = "10" oninput = "frameRateUpdate(this.value)"> 
+			<select id = "sortType" onchange="sortTypeUpdate(this.value)"> 
+				<option value = "BubbleSort"> Bubble Sort </option>
+				<option value = "QuickSort"> Quick Sort </option>
+				<option value = "SelectionSort"> Selection Sort </option>
+			</select>
 
-		<select id ="algorithms">
-				<option value = "Bubble Sort"> Bubble Sort </option>
-				<option value = "Insertion Sort"> Insertion Sort </option>
-				<option value = "Selection Sort"> Selection Sort </option>
-				<option value = "Quick Sort"> Quick Sort </option>
-			 	<option value="Merge Sort">Merge Sort</option>
-  			  	<option value="Heap Sort">Heap Sort</option>
- 				<option value="Counting Sort">Counting Sort</option>
-    			<option value="Radix Sort">Radix Sort</option>
-    	</select>
-    	<input type = "button" value = "Sort" id = "sortButton">
-	</nav>
-</header>
-	
-	<div id = "area">
-		<script src = "assets/js/sketch.js"> </script>
-	</div>
-
-
- 	<section id = "description">
- 		<p>
- 			<h2 id = "title"> </h2>
- 			<div id = "body">
-	 			
-	 			Quicksort is a divide and conquer sorting algorithm which works by dividing the array into sub-arrays with elements less than a selected partition to the left of the partition, and elements greater than the selected partition to the right of the partition. 
-	 			<br> 
-	 			<br>
-	 			<h4> Space Complexity:</h4> O(1) <br>
-	 			<h4> Worst Case Time Complexity: </h4> O(n<sup>2</sup>)<br>
-	 			<h4> Average Case Time Complexity: </h4> O(n<sup>2</sup>) <br>
-	 			<h4> Best Case Time Complexity: </h4> O(n<sup>2 </sup>) <br>
-	 			<h4> Stable: </h4> No <br>
-	 			<br>
- 			</div>
- 		</p>
- 	</section>
-
- 	<script src = "assets/js/main.js"> </script>
- 	<script src = "assets/js/p5.js"> </script>
- 	<script src = "assets/js/p5.sound.js"> </script>
+			<input type = "button" value ="submit" onClick = "executeSort()"> </button>
+		</form>
+	</header>
+	<main id = "main"> 
+		
+	</main>
+	<footer>
+		Created 2021, 09 26
+	</footer>
+		<script type="text/javascript" src="scripts/js/main.js"> </script>
 
 </body>
 </html>
-<?php>
-
