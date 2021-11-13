4aac331c22076b75ef5495d5f417e3f475d10ab4
Rachel Casey <rtkc.casey@gmail.com>
2021-11-13 00:56:12 -0700

    Significant code refactoring.
    Selection sort and settings classes implemented, selection sort encapsulated, need to update insertion sort and bubble sort in similar manners.

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,52 +1,128 @@
+
+
+
 let canvas;
-let elementCount = 50;
-let FPS = 60;
-let sortType = "BubbleSort";
+
+const Settings = {
+
+	_elementCount: 50, 
+	_FPS: 60, 
+	_sortType: "BubbleSort",
+	_lineThickness: 0,
+	_leftPadding: 0,
+	_backgroundColor: 100,
+	_framesPerUpdate: 10,
+	_sortExecution: false,
+
+	set _sortExecution(isSortExecution)
+	{
+		this._sortExecution = isSortExecution; 
+	},
+	get _sortExecution() 
+	{
+		return this._sortExecution;
+	},
+	set framesPerUpdate(newFPU)
+	{
+		this._framesPerUpdate = newFPU;
+	},
+	get framesPerUpdate()
+	{
+		return this._framesPerUpdate;
+	},
+	set backgroundColor(newBackgroundColor)
+	{
+		this._backgroundColor = newBackgroundColor;
+	},
+	get backgroundColor() {
+		return this._backgroundColor;
+	},
+	get height()
+	{
+		return $("#main").height();
+	},
+	get width()
+	{
+		return $("#main").width();
+	},
+	set leftPadding(newLeftPadding)
+	{
+		this._leftPadding = newLeftPadding;
+	},
+	get leftPadding() 
+	{
+		return this._leftPadding;
+	},
+	set lineThickness(newLineThickness)
+	{
+		this._lineThickness = newLineThickness;
+	},
+	get lineThickness()
+	{
+		return this._lineThickness;
+	},
+	set sortType(newSortType)
+	{
+		this._sortType = newSortType;
+	},
+	get sortType() 
+	{
+		return this._sortType;
+	},
+	set FPS(newFPS)
+	{
+		this._FPS = newFPS;
+	},
+	get FPS() 
+	{
+		return this._FPS;
+	},
+	set elementCount(newCount) {
+		this._elementCount = Number(newCount);
+	},
+	get elementCount() {
+		return this._elementCount;
+	}
+
+}
 
 let elements = [];
-
-let lineThickness;
+ 
 let sortedIndex = 0; 
-
-let leftPadding;
-let width; 
-let height;
 
 let savedIndex = 0;
 var bubbleIndex = 0;
 let savedMinimumValue = 10000;
 let savedMinimumIndex = 0;
-let backgroundColor = 100;
 let preparedSwap = false;
 
-let sortExecution = false;
-let framesPerUpdate = 10;
+
 
 function sortTypeUpdate(newSortType)
 {
-	sortType = newSortType;
-	console.log(sortType);
+	Settings.sortType = newSortType;
 }
 function elementCountUpdate(newElementCount)
 {
-	elementCount = newElementCount;
+	Settings.elementCount = newElementCount;
 	elements = [];
-	sortExecution = false;	
 	resetState();
-	for(let i = 0; i < elementCount; i++)
+	for(let i = 0; i < Settings.elementCount; i++)
 	{
 		elements[i] = max(getRandomInt(height), 5);
 	}
-		driver();
+		resetDraw();
+		drawElements(0, Settings.elementCount);
 }
+
 function frameRateUpdate(newFrameRate)
 {
-	framesPerUpdate = floor(60-newFrameRate) + 1;
+	Settings.framesPerUpdate = floor(60-newFrameRate) + 1;
 }
 
 function executeSort() 
 {
-	sortExecution = true;
+	Settings.sortExecution = true;
 }
 
 function resetState() 
@@ -58,6 +134,7 @@ function resetState()
 	sortedIndex = 0;
 
 }
+
 function getRandomInt(max) 
 {
   return Math.floor(Math.random() * max);
@@ -68,7 +145,7 @@ function windowResized()
 	height = $("#main").height();
 	width = $("#main").width();
 	resizeCanvas(width, height);
-	background(100);		
+	background(Settings.backgroundColor);		
 	savedMinimumValue = elements[savedIndex];
 
 	//Reset sort algo
@@ -80,15 +157,15 @@ function setup()
 	height = $("#main").height();
 	width = $("#main").width();
 	
-	canvas = createCanvas(width, height);
+	canvas = createCanvas(Settings.width, Settings.height);
 	canvas.parent("main");
 	canvas.style('z-index', -1);
-	background(backgroundColor);
-	for(let i = 0; i < elementCount; i++)
+	background(Settings.backgroundColor);
+	for(let i = 0; i < Settings.elementCount; i++)
 	{
-		elements[i] = max(getRandomInt(height), 5);
+		elements[i] = max(getRandomInt(Settings.Height), 5);
 	}
-	driver();
+	resetDraw(); 	
 }
 function swap(array, x, y)
 {
@@ -96,16 +173,17 @@ function swap(array, x, y)
 	array[x] = array[y];
 	array[y] = tmp;
 }
+
 function bubbleSort()
 {
 	if(!isSorted(elements))
 	{
-		background(backgroundColor);
-		drawElements(0, elementCount);
+		background(Settings.backgroundColor);
+		drawElements(0, Settings.elementCount);
 		savedIndex = bubbleSortVisualization(savedIndex);
 		if(preparedSwap)
 		{
-			drawElements(0, elementCount);
+			drawElements(0, Settings.elementCount);
 			drawElement(savedIndex-1, color(255,0,0), color(255));
 			drawElement(savedIndex, color(255,0,0), color(255));
 			swap(elements,savedIndex-1, savedIndex);
@@ -114,22 +192,21 @@ function bubbleSort()
 	}
 	else
 	{
-		background(backgroundColor);
-		drawElements(0, elementCount, color(0,255,0));
+		background(Settings.backgroundColor);
+		drawElements(0, Settings.elementCount, color(0,255,0));
 	}
 }
-
 
 function bubbleSortVisualization(index)
 {
 	preparedSwap = false;
-	if(index < elementCount-1)
+	if(index < Settings.elementCount-1)
 	{
-		for(let i = index; i < elementCount-1; i++)
+		for(let i = index; i < Settings.elementCount-1; i++)
 		{
 			if(elements[i] > elements[i+1])
 			{
-				background(backgroundColor);
+				background(Settings.backgroundColor);
 				drawElements(0, elements);
 				drawElement(i, color(255,0,0), color(255));
 				drawElement(i + 1,color(255,0,0), color(255));
@@ -138,8 +215,8 @@ function bubbleSortVisualization(index)
 			}
 			else
 			{
-				background(backgroundColor);
-				drawElements(0, elementCount);
+				background(Settings.backgroundColor);
+				drawElements(0, Settings.elementCount);
 				drawElement(i, color(0,0,255), color(255));
 				return i+1;
 			}
@@ -147,20 +224,74 @@ function bubbleSortVisualization(index)
 	}
 	return 0;
 }
+
 function insertionSort()
 {
+
+	if(!isSorted(elements))
+	{
+		if(sortedIndex === 0 && savedIndex === 0)
+	{
+		savedIndex = 1;
+		index = 1;
+	}
+		insertionSortVisualization();
+	}
+	else
+	{
+		background(Settings.backgroundColor);
+		drawElements(0, Settings.elementCount, color(0,255,0), color(255));
+	}
+
+
 }
+
+
+// DELETE THIS SHIT AND FIX THE GLOBAL ISSUE, THIS IS RIDICLOUS AND OF COURSE IT DOESNT WORK
+function insertionSortVisualization()
+{
+
+	if(savedIndex <= 0)
+	{
+		sortedIndex++;
+		savedIndex = sortedIndex;
+		return;
+
+	}
+	if(elements[savedIndex-1] > elements[savedIndex])
+	{
+		background(Settings.backgroundColor);
+		drawElements(0, Settings.elementCount);
+		drawElement(savedIndex-1, color(255, 0, 0), color(255));
+		drawElement(savedIndex, color(255, 0, 0), color(255));
+		swap(elements, savedIndex-1, savedIndex);
+		savedIndex++;
+		return;
+	}
+	else 
+	{
+		background(Settings.backgroundColor);
+		drawElements(0, Settings.elementCount);
+		drawElement(savedIndex, color(0,0,255), color(255));
+		sortedIndex++;
+		savedIndex = index;
+		return;
+
+	}
+}
+
 function resetDraw() 
 {
-	background(backgroundColor);
-	lineThickness = floor(width/elementCount); //Set thickness based on elementCount
+	background(Settings.backgroundColor);
+	Settings.lineThickness = floor(Settings.width/Settings.elementCount); //Set thickness based on elementCount
 	stroke(1); //Arbitary
-	leftPadding =  (width - (elementCount * lineThickness)) * 0.5;
+	Settings.leftPadding =  (Settings.width - (Settings.elementCount * Settings.lineThickness)) * 0.5;
 }
+
 function drawElement(index, color, oldColor)
 {
 	fill(color);
-	rect(leftPadding + lineThickness * index, height-elements[index], lineThickness, elements[index]);
+	rect(Settings.leftPadding + Settings.lineThickness * index, Settings.height-elements[index], Settings.lineThickness, elements[index]);
 	fill(oldColor);
 }
 
@@ -171,64 +302,94 @@ function drawElements(start, end, c = color(255), oc = color(255))
 			 drawElement(i, c, oc);
 		}
 }
-function selectionSort()
-{
-	if(!isSorted(elements))
-		{
-			background(backgroundColor);
-			selectionSortVisualization(savedIndex);
-			savedIndex++;
-		}
-		else 
-		{	
-			background(backgroundColor)
-			drawElements(0, elementCount, color(0, 255, 0));
-		}
-}
-function selectionSortVisualization(index)
-{
-	if(index < elementCount)
+
+class SelectionSort {
+
+	constructor()
 	{
-		let minValue = elements[sortedIndex];
-		let minIndex = sortedIndex;
-		for(index; index < elementCount; index++)
+		this._outsideIndex = 0;
+		this._insideIndex = 0;
+		this._minimumIndex = 0;
+
+	}
+
+	set outsideIndex(newOutsideIndex) { this._outsideIndex = newOutsideIndex;}
+	get outsideIndex(){return this._outsideIndex;}
+	set insideIndex(newInsideIndex){this._insideIndex = newInsideIndex;}
+	get insideIndex() {return this._insideIndex;}
+	set minimumIndex(newMinimumIndex) { this._minimumIndex = newMinimumIndex }
+	get minimumIndex() { return this._minimumIndex; }
+	
+
+	drawSwap()
+	{
+		background(Settings.backgroundColor);
+		drawElements(0, Settings.elementCount);
+		drawElements(0, this.outsideIndex, color(0, 255,0), color(255));
+		drawElement(this.outsideIndex, color(255,0,0), color(255));
+		drawElement(this.minimumIndex, color(255,0,0), color(255));
+	}
+
+	drawHit() 
+	{
+		background(Settings.backgroundColor);
+		drawElements(0, Settings.elementCount);
+		drawElements(0, this.outsideIndex, color(0, 255,0), color(255));
+		drawElement(this.insideIndex, color(255,255,0), color(255))
+		drawElement(this.outsideIndex, color(0, 255, 0), color(255));
+	}
+
+	drawScan() 
+	{
+		background(Settings.backgroundColor);
+		drawElements(0, Settings.elementCount);
+		drawElements(0, this.outsideIndex, color(0, 255,0), color(255));
+		drawElement(this.minimumIndex, color(255, 255, 0), color(255));
+		drawElement(this.insideIndex, color(0,0,255), color(255));
+		drawElement(this.outsideIndex, color(0, 255, 0), color(255));
+	}
+
+	drawComplete()
+	{
+		background(Settings.backgroundColor);
+		drawElements(0, Settings.elementCount, color(0,255,0), color(255));
+	}
+	
+	sort()
+	{
+
+		if(this.outsideIndex > this.elementCount)
 		{
-			if(elements[index] < savedMinimumValue)
+			this.drawComplete();
+			return;
+		}
+		
+		if(this.insideIndex > Settings.elementCount)
+		{
+			this.drawSwap();
+			swap(elements, this.outsideIndex, this.minimumIndex);
+			this.outsideIndex++;
+			this.insideIndex = this.outsideIndex;
+			this.minimumIndex = this.outsideIndex;
+			return;
+		}
+		else
+		{
+			if(elements[this.insideIndex] <= elements[this.minimumIndex])
 			{
-				savedMinimumValue = elements[index];
-				savedMinimumIndex = index;
-				background(backgroundColor);
-				drawElements(0, elementCount);
-				drawElement(index, color(0, 255, 0), color(255));
-				return index;
+				this.drawHit();
+				this.minimumIndex = this.insideIndex;
+				this.insideIndex++;
+				return;
 			}
 			else
 			{
-				background(backgroundColor);
-				drawElements(0, elementCount);
-				drawElement(savedMinimumIndex, color(0, 255, 0), color(255))
-				drawElement(index, color(0, 0, 255), color(255));
-				return index;
+				this.drawScan();
+				this.insideIndex++;
+				return;
 			}
 		}
 	}
-	else
-	{
-		drawElements(0, elementCount);
-		drawElement(savedMinimumIndex, color(255, 0, 0), color(255));
-		drawElement(sortedIndex, color(255, 0, 0), color(255));
-		swap(elements, sortedIndex, savedMinimumIndex);
-		sortedIndex++;
-		savedMinimumIndex = sortedIndex;
-		savedMinimumValue = elements[sortedIndex];
-		savedIndex = sortedIndex;
-	}
-}
-function driver()
-{
-	resetDraw();
-	drawElements(0, elementCount);
-//	sortElements();
 }
 function isSorted(array) 
 {
@@ -241,23 +402,27 @@ function isSorted(array)
 	}
 	return true;
 }
+
+
+let SelectionSorter = new SelectionSort();
+
 function draw() 
 {
 	//resetDraw();
 	//drawElements(0, elementCount-1);
-	if(sortExecution) 
+	if(Settings.sortExecution) 
 	{
-		if(frameCount % framesPerUpdate == 0) 
+		if(frameCount % Settings.framesPerUpdate == 0) 
 		{
-			if(sortType == "SelectionSort")
+			if(Settings.sortType == "SelectionSort")
 			{
-				selectionSort();
+			 	SelectionSorter.sort();
 			}
-			if(sortType == "BubbleSort")
+			if(Settings.sortType == "BubbleSort")
 			{
 				bubbleSort();
 			}
-			if(sortType == "InsertionSort")
+			if(Settings.sortType == "InsertionSort")
 			{
 				insertionSort();
 			}
